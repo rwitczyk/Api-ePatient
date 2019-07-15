2d521b152d4d7ee83b451d7abc2c2e3e43dc3d32
rwitczyk <r.witczyk@wp.pl>
2019-07-15 13:31:19 -0400

    Init SpringBoot Project

---
+++ src/main/java/com/ePatient/EPatientApplication.java
@@ -0,0 +1,13 @@
+package com.ePatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPatientApplication.class, args);
+	}
+
+}
